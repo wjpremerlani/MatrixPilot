0f03d7f4b14ae72522a9635b1028b8dc9189509f
William Premerlani <wjpremerlani@gmail.com>
2019-04-24 19:36:58 -0400

    first step in improving accuracy of conversion of gps COG, SOG to X-Y: upgrading resolution of actual_dir from 8 bits to 16 bits

--- libDCM/mathlibNAV.h
+++ libDCM/mathlibNAV.h
@@ -18,6 +18,7 @@
 // You should have received a copy of the GNU General Public License
 // along with MatrixPilot.  If not, see <http://www.gnu.org/licenses/>.
 
+uint16_t gps_cog_to_16bit_circular(uint16_t angle);
 
 int16_t sine(int8_t angle);
 int8_t arcsine(int16_t y);  // arcsine takes the y coordinate of an x,y point and returns an angle
