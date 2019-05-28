39ded9a0f053efca685d15895228caf02e225fa8
wjpremerlani@gmail.com <wjpremerlani@gmail.com>
2019-05-28 12:43:12 -0400

    added waypoints for easystar for both the soccer field and Albany airport

---
+++ Config/flightplan-waypoints_easystar_alb_airport.h
@@ -0,0 +1,22 @@
+
+
+#define CAM_VIEW_LAUNCH         { 0, 0, 0 }
+
+
+const struct waypointDef waypoints[] = {
+	{ { -738042715, 427377643, 50 } , 20 , 0 , F_ABSOLUTE , CAM_VIEW_LAUNCH } , //Waypoint 1
+	{ { -738043360, 427393876, 50 } , 20 , 0 , F_ABSOLUTE , CAM_VIEW_LAUNCH } , //Waypoint 2
+	{ { -738031343, 427393875, 50 } , 20 , 0 , F_ABSOLUTE , CAM_VIEW_LAUNCH } , //Waypoint 3
+	{ { -738029841, 427364010, 50 } , 20 , 0 , F_ABSOLUTE , CAM_VIEW_LAUNCH } , //Waypoint 4
+	{ { -738042070, 427363852, 50 } , 20 , 0 , F_ABSOLUTE , CAM_VIEW_LAUNCH } , //Waypoint 5
+};
+
+
+
+const struct waypointDef rtlWaypoints[] = {
+	{ { -738042715, 427377643, 50 } , 20 , 0 , F_ABSOLUTE , CAM_VIEW_LAUNCH } , //Waypoint 1
+	{ { -738043360, 427393876, 50 } , 20 , 0 , F_ABSOLUTE , CAM_VIEW_LAUNCH } , //Waypoint 2
+	{ { -738031343, 427393875, 50 } , 20 , 0 , F_ABSOLUTE , CAM_VIEW_LAUNCH } , //Waypoint 3
+	{ { -738029841, 427364010, 50 } , 20 , 0 , F_ABSOLUTE , CAM_VIEW_LAUNCH } , //Waypoint 4
+	{ { -738042070, 427363852, 50 } , 20 , 0 , F_ABSOLUTE , CAM_VIEW_LAUNCH } , //Waypoint 5
+};
