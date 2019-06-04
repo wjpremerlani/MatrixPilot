12ca5955206415fc64c7cc9b366b35563ca20def
William Premerlani <wjpremerlani@gmail.com>
2019-06-04 18:54:08 -0400

    changed the SPEED_MARGIN parameter for the EasyStar from 4 that was being used for the Cirrus jet, to 0, which is a better value for the Easy Star, to get better speed control. Reduced HEIGHT_MARGIN from 20 to 15 for the EasyStar to get better altitude and speed control. Tweaked the waypoints a bit to improve the auto land.

--- Config/flightplan-waypoints.h
+++ Config/flightplan-waypoints.h
@@ -8,14 +8,16 @@ const struct waypointDef waypoints[] = {
 	{ { -125, 105, 75 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 2
 	{ { -66, 182, 75 } , 10 , 0 , F_NORMAL , CAM_VIEW_LAUNCH } , //Waypoint 3
 	{ { 155, 20, 75 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 4
-	{ { 96, -66, 75 } , 10 , 0 , F_NORMAL , CAM_VIEW_LAUNCH } , //Waypoint 5
-	{ { 0, 0, 75 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 6
-	{ { -125, 105, 75 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 7
-	{ { -67, 182, 75 } , 10 , 0 , F_NORMAL  , CAM_VIEW_LAUNCH } , //Waypoint 8
-	{ { 155, 20, 50 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 9
-	{ { 94, -65, 30 } , 10 , 0 , F_NORMAL , CAM_VIEW_LAUNCH } , //Waypoint 10
-	{ { 0, 0, 3 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 11
-	{ { -125, 105, -5 } , 0 , 0 , F_LAND + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 12
+	{ { 165, -52, 75 } , 10 , 0 , F_NORMAL , CAM_VIEW_LAUNCH } , //Waypoint 5
+	{ { 96, -66, 75 } , 10 , 0 , F_NORMAL , CAM_VIEW_LAUNCH } , //Waypoint 6
+	{ { 0, 0, 75 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 7
+	{ { -125, 105, 75 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 8
+	{ { -67, 182, 75 } , 10 , 0 , F_NORMAL , CAM_VIEW_LAUNCH } , //Waypoint 9
+	{ { 155, 20, 50 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 10
+	{ { 165, -52, 35 } , 10 , 0 , F_NORMAL , CAM_VIEW_LAUNCH } , //Waypoint 11
+	{ { 94, -65, 20 } , 10 , 0 , F_NORMAL , CAM_VIEW_LAUNCH } , //Waypoint 12
+	{ { -5, 3, 5 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 13
+	{ { -125, 105, -10 } , 0 , 0 , F_LAND + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 14
 };
 
 
@@ -25,12 +27,15 @@ const struct waypointDef rtlWaypoints[] = {
 	{ { -125, 105, 75 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 2
 	{ { -66, 182, 75 } , 10 , 0 , F_NORMAL , CAM_VIEW_LAUNCH } , //Waypoint 3
 	{ { 155, 20, 75 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 4
-	{ { 96, -66, 75 } , 10 , 0 , F_NORMAL , CAM_VIEW_LAUNCH } , //Waypoint 5
-	{ { 0, 0, 75 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 6
-	{ { -125, 105, 75 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 7
-	{ { -67, 182, 75 } , 10 , 0 , F_NORMAL  , CAM_VIEW_LAUNCH } , //Waypoint 8
-	{ { 155, 20, 50 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 9
-	{ { 94, -65, 30 } , 10 , 0 , F_NORMAL , CAM_VIEW_LAUNCH } , //Waypoint 10
-	{ { 0, 0, 3 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 11
-	{ { -125, 105, -5 } , 0 , 0 , F_LAND + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 12
+	{ { 165, -52, 75 } , 10 , 0 , F_NORMAL , CAM_VIEW_LAUNCH } , //Waypoint 5
+	{ { 96, -66, 75 } , 10 , 0 , F_NORMAL , CAM_VIEW_LAUNCH } , //Waypoint 6
+	{ { 0, 0, 75 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 7
+	{ { -125, 105, 75 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 8
+	{ { -67, 182, 75 } , 10 , 0 , F_NORMAL , CAM_VIEW_LAUNCH } , //Waypoint 9
+	{ { 155, 20, 50 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 10
+	{ { 165, -52, 35 } , 10 , 0 , F_NORMAL , CAM_VIEW_LAUNCH } , //Waypoint 11
+	{ { 94, -65, 20 } , 10 , 0 , F_NORMAL , CAM_VIEW_LAUNCH } , //Waypoint 12
+	{ { -5, 3, 5 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 13
+	{ { -125, 105, -10 } , 0 , 0 , F_LAND + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 14
 };
+
