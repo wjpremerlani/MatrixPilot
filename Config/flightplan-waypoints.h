7b8adf5d09e75875bad08d9f2db9bceeb67a06b9
William Premerlani <wjpremerlani@gmail.com>
2019-06-05 19:50:36 -0400

    added a flyover waypoint pattern at the soccer field

--- Config/flightplan-waypoints.h
+++ Config/flightplan-waypoints.h
@@ -13,11 +13,11 @@ const struct waypointDef waypoints[] = {
 	{ { 0, 0, 75 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 7
 	{ { -125, 105, 75 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 8
 	{ { -67, 182, 75 } , 10 , 0 , F_NORMAL , CAM_VIEW_LAUNCH } , //Waypoint 9
-	{ { 155, 20, 50 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 10
-	{ { 165, -52, 35 } , 10 , 0 , F_NORMAL , CAM_VIEW_LAUNCH } , //Waypoint 11
+	{ { 155, 20, 45 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 10
+	{ { 165, -52, 30 } , 10 , 0 , F_NORMAL , CAM_VIEW_LAUNCH } , //Waypoint 11
 	{ { 94, -65, 20 } , 10 , 0 , F_NORMAL , CAM_VIEW_LAUNCH } , //Waypoint 12
-	{ { -5, 3, 5 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 13
-	{ { -125, 105, -10 } , 0 , 0 , F_LAND + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 14
+	{ { -5, 3, 10 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 13
+	{ { -125, 105, -5 } , 0 , 0 , F_LAND + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 14
 };
 
 
@@ -32,10 +32,10 @@ const struct waypointDef rtlWaypoints[] = {
 	{ { 0, 0, 75 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 7
 	{ { -125, 105, 75 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 8
 	{ { -67, 182, 75 } , 10 , 0 , F_NORMAL , CAM_VIEW_LAUNCH } , //Waypoint 9
-	{ { 155, 20, 50 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 10
-	{ { 165, -52, 35 } , 10 , 0 , F_NORMAL , CAM_VIEW_LAUNCH } , //Waypoint 11
+	{ { 155, 20, 45 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 10
+	{ { 165, -52, 30 } , 10 , 0 , F_NORMAL , CAM_VIEW_LAUNCH } , //Waypoint 11
 	{ { 94, -65, 20 } , 10 , 0 , F_NORMAL , CAM_VIEW_LAUNCH } , //Waypoint 12
-	{ { -5, 3, 5 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 13
-	{ { -125, 105, -10 } , 0 , 0 , F_LAND + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 14
+	{ { -5, 3, 10 } , 10 , 0 , F_NORMAL + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 13
+	{ { -125, 105, -5 } , 0 , 0 , F_LAND + F_CROSS_TRACK , CAM_VIEW_LAUNCH } , //Waypoint 14
 };
 
