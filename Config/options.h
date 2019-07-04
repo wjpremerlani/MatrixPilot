fd333b4aaa85c0f8a7740c1b88b3c028020256d5
William Premerlani <wjpremerlani@gmail.com>
2019-07-04 15:51:38 -0400

    revert changes in deadReckoning.c and estLocation.c in preparation for testing with Ublox

--- Config/options.h
+++ Config/options.h
@@ -119,7 +119,7 @@
 
 ////////////////////////////////////////////////////////////////////////////////
 // Set this value to your GPS type.  (Set to GPS_STD, GPS_UBX_2HZ, GPS_UBX_4HZ, GPS_MTEK, GPS_NMEA, or GPS_NONE)
-#define GPS_TYPE                            GPS_STD
+#define GPS_TYPE                            GPS_UBX_4HZ
 //#define DEFAULT_GPS_BAUD                    57600   // added for GPS_NMEA support
 
 
