ce2b12c49276533aabec16286ec1d282e3798d8f
William Premerlani <wjpremerlani@gmail.com>
2023-07-25 14:24:40 -0400

    luge_imu : recorded accelerometer offsets and gains and gyro gains for sn104 and sn105

--- MultiRotor/Config/options_LUGE7_SN104.h
+++ MultiRotor/Config/options_LUGE7_SN104.h
@@ -4,17 +4,17 @@
 #define	options_LUGE7_SN104_H
 
 #define CUSTOM_OFFSETS
-#define XACCEL_OFFSET	( 0 )
-#define YACCEL_OFFSET	( 0 )
-#define ZACCEL_OFFSET	( 0 )
+#define XACCEL_OFFSET	( 12 )
+#define YACCEL_OFFSET	( -50 )
+#define ZACCEL_OFFSET	( -28 )
 #define GYRO_OFFSET_TABLE "../libUDB/gyro_tables/table_luge7_sn104.h"
-#define CALIBRATIONX	1.0000
-#define CALIBRATIONY	1.0000
-#define CALIBRATIONZ	1.0000
+#define CALIBRATIONX	1.0167
+#define CALIBRATIONY	1.0175
+#define CALIBRATIONZ	1.0184
 #define CALIB_GRAVITY	4096
-#define CAL_GRAV_X	4096
-#define CAL_GRAV_Y	4096
-#define CAL_GRAV_Z	4096
+#define CAL_GRAV_X	4109
+#define CAL_GRAV_Y	4101
+#define CAL_GRAV_Z	4083
 
 #endif	/* options_LUGE7_SN104_H */
 
--- MultiRotor/Config/options_LUGE7_SN105.h
+++ MultiRotor/Config/options_LUGE7_SN105.h
@@ -4,17 +4,17 @@
 #define	options_LUGE7_SN105_H
 
 #define CUSTOM_OFFSETS
-#define XACCEL_OFFSET	( 0 )
-#define YACCEL_OFFSET	( 0 )
-#define ZACCEL_OFFSET	( 0 )
+#define XACCEL_OFFSET	( 27 )
+#define YACCEL_OFFSET	( -61 )
+#define ZACCEL_OFFSET	( -30 )
 #define GYRO_OFFSET_TABLE "../libUDB/gyro_tables/table_luge7_sn105.h"
-#define CALIBRATIONX	1.0000
-#define CALIBRATIONY	1.0000
-#define CALIBRATIONZ	1.0000
+#define CALIBRATIONX	1.0132
+#define CALIBRATIONY	1.0184
+#define CALIBRATIONZ	1.0129
 #define CALIB_GRAVITY	4096
-#define CAL_GRAV_X	4096
-#define CAL_GRAV_Y	4096
-#define CAL_GRAV_Z	4096
+#define CAL_GRAV_X	4108
+#define CAL_GRAV_Y	4106
+#define CAL_GRAV_Z	4075
 
 #endif	/* options_LUGE7_SN105_H */
 
