978e63c593ac1dbc98cfded79a7d25b90aa4c3c6
William Premerlani <wjpremerlani@gmail.com>
2023-08-26 15:48:52 -0400

    luge_imu : recorded gyro temperature compensation tables for sn112, 113, 114 and 115

--- MultiRotor/Config/options.h
+++ MultiRotor/Config/options.h
@@ -32,8 +32,8 @@
 //#define RECORD_OFFSETS        // record raw accelerometer data
 //#define BUILD_OFFSET_TABLE    // builds gyro temperature compensation table
 //#define DEBUG_TABLE_BUILD
-//#define GYRO_OFFSETS          // verifies gyro temperature compensation table
-#define GYRO_CALIB            // used to compute gyro calibration
+#define GYRO_OFFSETS          // verifies gyro temperature compensation table
+//#define GYRO_CALIB            // used to compute gyro calibration
 
 // the following are options used for development and debugging activities
 //#define TEST_LOGGER_HZ
@@ -90,11 +90,11 @@
 //#define LUGE7_SN108
 //#define LUGE7_SN109
 //#define LUGE7_SN110 // not in service, wifi issue at low temp
-#define LUGE7_SN111
+//#define LUGE7_SN111
 //#define LUGE7_SN112
 //#define LUGE7_SN113
 //#define LUGE7_SN114
-//#define LUGE7_SN115
+#define LUGE7_SN115
 //#define LUGE7_SN116
 //#define LUGE7_SN117
 //#define LUGE7_SN118
