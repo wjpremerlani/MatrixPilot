2089e124c1db7c5a9faef574df2dfb7b149c6295
William Premerlani <wjpremerlani@gmail.com>
2023-10-10 20:34:07 -0400

    luge_imu : revised the cross coupling factors for sns 124, 125 and 126

--- MultiRotor/Config/options_LUGE7_SN124.h
+++ MultiRotor/Config/options_LUGE7_SN124.h
@@ -15,7 +15,9 @@
 #define CAL_GRAV_Y	4102
 #define CAL_GRAV_Z	4069
 
-// it was not possible to determine cross coupling factors, data was chaotic
+#define X_CROSS_COUPLING 22
+#define Y_CROSS_COUPLING 0
+#define Z_CROSS_COUPLING 0
 
 #endif	/* options_LUGE7_SN124_H */
 
--- MultiRotor/Config/options_LUGE7_SN126.h
+++ MultiRotor/Config/options_LUGE7_SN126.h
@@ -15,7 +15,9 @@
 #define CAL_GRAV_Y	4097
 #define CAL_GRAV_Z	4072
 
-// it was not possible to determine cross coupling factors, data was chaotic
+#define X_CROSS_COUPLING 16
+#define Y_CROSS_COUPLING 0
+#define Z_CROSS_COUPLING 0
 
 #endif	/* options_LUGE7_SN126_H */
 
