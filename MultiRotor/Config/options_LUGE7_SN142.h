290dc37e39b329b4d826bd0664ad6edfc32c7fb7
William Premerlani <wjpremerlani@gmail.com>
2023-10-26 16:37:12 -0400

    luge_imu : recorded accelerometer offsets and gains for serial numbers 135 through 142

--- MultiRotor/Config/options_LUGE7_SN142.h
+++ MultiRotor/Config/options_LUGE7_SN142.h
@@ -3,17 +3,17 @@
 #define	options_LUGE7_SN142_H
 
 #define CUSTOM_OFFSETS
-#define XACCEL_OFFSET	( 0 )
-#define YACCEL_OFFSET	( 0 )
-#define ZACCEL_OFFSET	( 0 )
+#define XACCEL_OFFSET	( -28 )
+#define YACCEL_OFFSET	( -46)
+#define ZACCEL_OFFSET	( -18 )
 #define GYRO_OFFSET_TABLE "../libUDB/gyro_tables/table_luge7_sn142.h"
 #define CALIBRATIONX	1.0134
 #define CALIBRATIONY	1.0134
 #define CALIBRATIONZ	1.0134
 #define CALIB_GRAVITY	4096
-#define CAL_GRAV_X	4096
-#define CAL_GRAV_Y	4096
-#define CAL_GRAV_Z	4096
+#define CAL_GRAV_X	4117
+#define CAL_GRAV_Y	4104
+#define CAL_GRAV_Z	4071
 
 #define X_CROSS_COUPLING 0
 #define Y_CROSS_COUPLING 0
