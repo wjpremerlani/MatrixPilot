07ffa9995aa5aff2641b9d575c0eea4df2a20b91
William Premerlani <wjpremerlani@gmail.com>
2023-11-07 15:58:31 -0500

    luge_imu : recorded gyro offset table and accelerometer offsets and calibrations for serial numbers 155-158

--- MultiRotor/Config/options.h
+++ MultiRotor/Config/options.h
@@ -31,9 +31,9 @@
 #define RESIDUAL_LOG_PERIOD 1  // 60 times per minute
 
 // the following are used to measure offsets and gains for commissioning
-//#define RECORD_OFFSETS        // record raw accelerometer data
-#define BUILD_OFFSET_TABLE    // builds gyro temperature compensation table
-#define DEBUG_TABLE_BUILD
+#define RECORD_OFFSETS        // record raw accelerometer data
+//#define BUILD_OFFSET_TABLE    // builds gyro temperature compensation table
+//#define DEBUG_TABLE_BUILD
 //#define GYRO_OFFSETS          // verifies gyro temperature compensation table
 //#define GYRO_CALIB            // used to compute gyro calibration
 //#define CROSS_COUPLING          // measure cross coupling
--- MultiRotor/Config/options_LUGE7_SN156.h
+++ MultiRotor/Config/options_LUGE7_SN156.h
@@ -3,17 +3,17 @@
 #define	options_LUGE7_SN156_H
 
 #define CUSTOM_OFFSETS
-#define XACCEL_OFFSET	( 0 )
-#define YACCEL_OFFSET	( 0 )
-#define ZACCEL_OFFSET	( 0 )
+#define XACCEL_OFFSET	( -10 )
+#define YACCEL_OFFSET	( -49 )
+#define ZACCEL_OFFSET	( -28 )
 #define GYRO_OFFSET_TABLE "../libUDB/gyro_tables/table_luge7_sn156.h"
 #define CALIBRATIONX	1.0134
 #define CALIBRATIONY	1.0134
 #define CALIBRATIONZ	1.0134
 #define CALIB_GRAVITY	4096
-#define CAL_GRAV_X	4096
-#define CAL_GRAV_Y	4096
-#define CAL_GRAV_Z	4096
+#define CAL_GRAV_X	4103
+#define CAL_GRAV_Y	4099
+#define CAL_GRAV_Z	4080
 
 #define X_CROSS_COUPLING 0
 #define Y_CROSS_COUPLING 0
