0032ea7420da65697637734dc56e9138fe437422
William Premerlani <wjpremerlani@gmail.com>
2023-11-18 14:19:26 -0500

    luge_imu : created files for serial numbers 162, 163 and 164 in the hopes of salvaging a few boards from the "penalty" box

--- MultiRotor/Config/options.h
+++ MultiRotor/Config/options.h
@@ -20,7 +20,7 @@
 
 // the following defines select what gets sent to the logger
 //#define LOG_IMU_WP1               // logs IMU data during a run for wolf_pac version 1
-#define LOG_IMU_WP2               // logs IMU data during a run for wolf_pac version 2
+//#define LOG_IMU_WP2               // logs IMU data during a run for wolf_pac version 2
 //#define START_TRACK_LOG         // logs 2 components of force and pitch
 //#define LOG_RESIDUALS         // logs residual offsets between runs
 //#define RESIDUAL_LOG_PERIOD 30  // 2 times per minute
@@ -32,8 +32,8 @@
 
 // the following are used to measure offsets and gains for commissioning
 //#define RECORD_OFFSETS        // record raw accelerometer data
-//#define BUILD_OFFSET_TABLE    // builds gyro temperature compensation table
-//#define DEBUG_TABLE_BUILD
+#define BUILD_OFFSET_TABLE    // builds gyro temperature compensation table
+#define DEBUG_TABLE_BUILD
 //#define GYRO_OFFSETS          // verifies gyro temperature compensation table
 //#define GYRO_CALIB            // used to compute gyro calibration
 //#define CROSS_COUPLING          // measure cross coupling
@@ -51,7 +51,7 @@
 
 // When using USE_PACKETIZED_TELEMERTY, baud will be 460800, and some 
 // non-printable characters are written out, as header bytes for packets.
-#define USE_PACKETIZED_TELEMERTY
+//#define USE_PACKETIZED_TELEMERTY
 
 
 // Enable UDB7LUGE in a Board definition ifdef block below for new WolfPac1 wifi boards
@@ -74,7 +74,7 @@
 
 
 // set the logger hertz, allowable values are 1,2,4,5,10,20,25,40,50,100 or 200
-#define LOGGER_HZ	100
+#define LOGGER_HZ	1
 #define HEADER_HZ	20          // records per second during header logging
 #define SLIDE_DET_HZ	200     // computations per second to detect beginning of a run
 //#define TILT_STOP_DELAY 10      // delay in seconds to allow for a roll over
@@ -85,7 +85,7 @@
 // select a wolf_pac by defining its internal label
 //#define LUGE7_SNnew // used to program a WP without a serial number
 
-#define LUGE7_SN050
+//#define LUGE7_SN050
 //#define LUGE7_SN051
 //#define LUGE7_SN052
 //#define LUGE7_SN053
@@ -129,14 +129,13 @@
 //#define LUGE7_SN136
 //#define LUGE7_SN137
 //#define LUGE7_SN138
-//#define LUGE7_SN139
+//#error #define LUGE7_SN139 //out of service too much drift
 //#define LUGE7_SN140
 //#define LUGE7_SN141
 //#define LUGE7_SN142
-
 //#define LUGE7_SN143
 //#define LUGE7_SN144
-//#define LUGE7_SN145
+//#error #define LUGE7_SN145 out of service
 //#define LUGE7_SN146
 //#define LUGE7_SN147
 //#define LUGE7_SN148
@@ -157,6 +156,7 @@
 //#define LUGE7_SN162
 //#define LUGE7_SN163
 //#define LUGE7_SN164
+
 //#define LUGE7_SN165
 //#define LUGE7_SN166
 
@@ -981,6 +981,45 @@
 #define LOG_EULER
 #include "options_LUGE7_SN161.h"
 #endif // LUGE7_SN161
+
+#ifdef LUGE7_SN162
+#define ENABLE_ESP32
+#define UDB7LUGE
+#define BOARD LUGE7
+#define SERIAL_NUMBERD1	1
+#define SERIAL_NUMBERD2	6
+#define SERIAL_NUMBERD3 2
+#define ACCEL_RANGE         8
+#define GYRO_RANGE	    1000
+#define LOG_EULER
+#include "options_LUGE7_SN162.h"
+#endif // LUGE7_SN162
+
+#ifdef LUGE7_SN163
+#define ENABLE_ESP32
+#define UDB7LUGE
+#define BOARD LUGE7
+#define SERIAL_NUMBERD1	1
+#define SERIAL_NUMBERD2	6
+#define SERIAL_NUMBERD3 3
+#define ACCEL_RANGE         8
+#define GYRO_RANGE	    1000
+#define LOG_EULER
+#include "options_LUGE7_SN163.h"
+#endif // LUGE7_SN163
+
+#ifdef LUGE7_SN164
+#define ENABLE_ESP32
+#define UDB7LUGE
+#define BOARD LUGE7
+#define SERIAL_NUMBERD1	1
+#define SERIAL_NUMBERD2	6
+#define SERIAL_NUMBERD3 4
+#define ACCEL_RANGE         8
+#define GYRO_RANGE	    1000
+#define LOG_EULER
+#include "options_LUGE7_SN164.h"
+#endif // LUGE7_SN164
 
 
 #ifdef LUGE7_SN050
