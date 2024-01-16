cc793362a09d907647ffe283863bc41cb1f3a4d0
William Premerlani <wjpremerlani@gmail.com>
2024-01-16 10:54:37 -0500

    luge_imu : recorded residual offsets for sn156

--- libUDB/gyro_tables/table_luge7_sn156.h
+++ libUDB/gyro_tables/table_luge7_sn156.h
@@ -9,7 +9,7 @@
 
 #define TABLE_ORIGIN  (- 14512 )
 
-int16_t residual_offset[] = { 0 , 0 , 0 } ;
+int16_t residual_offset[] = { 355 , 196 , -29 } ;
 
 
 const struct gyro_offset_table_entry gyro_offset_table[] = {
