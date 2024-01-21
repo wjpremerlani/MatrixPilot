8a772605968da6902cf54cee7135111a395a580c
William Premerlani <wjpremerlani@gmail.com>
2024-01-21 14:11:47 -0500

    luge_imu : recorded accelerometer offsets and calibration factors for sns 164-167

--- MultiRotor/Config/options.h
+++ MultiRotor/Config/options.h
@@ -33,9 +33,9 @@
 //#define RESIDUAL_LOG_PERIOD 1  // 60 times per minute
 
 // the following are used to measure offsets and gains for commissioning
-//#define RECORD_OFFSETS        // record raw accelerometer data
-#define BUILD_OFFSET_TABLE    // builds gyro temperature compensation table
-#define DEBUG_TABLE_BUILD
+#define RECORD_OFFSETS        // record raw accelerometer data
+//#define BUILD_OFFSET_TABLE    // builds gyro temperature compensation table
+//#define DEBUG_TABLE_BUILD
 //#define GYRO_OFFSETS          // verifies gyro temperature compensation table
 //#define GYRO_CALIB            // used to compute gyro calibration
 //#define CROSS_COUPLING          // measure cross coupling
@@ -167,7 +167,7 @@
 //#define LUGE7_SN164
 //#define LUGE7_SN165
 //#define LUGE7_SN166
-//#define LUGE7_SN167
+#define LUGE7_SN167
 
 
 //#define LUGE7_SN168
@@ -188,7 +188,7 @@
 //#define LUGE7_SN180
 //#define LUGE7_SN181
 //#define LUGE7_SN182
-#define LUGE7_SN183
+//#define LUGE7_SN183
 
 
 //#define MINI6_SN4 // SN1, Tucker West
--- MultiRotor/Config/options_LUGE7_SN164.h
+++ MultiRotor/Config/options_LUGE7_SN164.h
@@ -3,17 +3,17 @@
 #define	options_LUGE7_SN164_H
 
 #define CUSTOM_OFFSETS
-#define XACCEL_OFFSET	( 0 )
-#define YACCEL_OFFSET	( 0 )
-#define ZACCEL_OFFSET	( 0 )
+#define XACCEL_OFFSET	( 5 )
+#define YACCEL_OFFSET	( -42 )
+#define ZACCEL_OFFSET	( 9 )
 #define GYRO_OFFSET_TABLE "../libUDB/gyro_tables/table_luge7_sn164.h"
 #define CALIBRATIONX	1.0134
 #define CALIBRATIONY	1.0134
 #define CALIBRATIONZ	1.0134
 #define CALIB_GRAVITY	4096
-#define CAL_GRAV_X	4096
-#define CAL_GRAV_Y	4096
-#define CAL_GRAV_Z	4096
+#define CAL_GRAV_X	4098
+#define CAL_GRAV_Y	4102
+#define CAL_GRAV_Z	4099
 
 #define X_CROSS_COUPLING 0
 #define Y_CROSS_COUPLING 0
