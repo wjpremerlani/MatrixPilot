896fc4350ab873c71c329d7a09f81f0b1a9ffc5d
William Premerlani <wjpremerlani@gmail.com>
2024-02-14 13:58:20 -0500

    luge_imu_strain_tests : added files for two boards used for research

---
+++ libUDB/gyro_tables/table_luge7_snts1.h
@@ -0,0 +1,22 @@
+
+
+#ifndef TABLE_LUGE7_SNts1_H
+#define	TABLE_LUGE7_SNts1_H
+
+// used in LUGE7_SNts1 partition size 64
+
+#define STEP_SIZE 64
+
+#define TABLE_ORIGIN  ( 0 )
+
+int16_t residual_offset[] = { 0 , 0 , 0 } ;
+
+
+const struct gyro_offset_table_entry gyro_offset_table[] = {
+{	0	,	0	,	0	},
+{	0	,	0	,	0	},
+{	0	,	0	,	0	}
+};
+
+#endif	/* TABLE_LUGE7_SNts1_H */
+
---
+++ libUDB/gyro_tables/table_luge7_snts2.h
@@ -0,0 +1,22 @@
+
+
+#ifndef TABLE_LUGE7_SNts2_H
+#define	TABLE_LUGE7_SNts2_H
+
+// used in LUGE7_SNts2 partition size 64
+
+#define STEP_SIZE 64
+
+#define TABLE_ORIGIN  ( 0 )
+
+int16_t residual_offset[] = { 0 , 0 , 0 } ;
+
+
+const struct gyro_offset_table_entry gyro_offset_table[] = {
+{	0	,	0	,	0	},
+{	0	,	0	,	0	},
+{	0	,	0	,	0	}
+};
+
+#endif	/* TABLE_LUGE7_SNts2_H */
+
