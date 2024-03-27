a8e12ac5deeca377529a07e19bb1cb5fafcd42c3
William Premerlani <wjpremerlani@gmail.com>
2024-03-27 17:20:25 -0400

    luge_imu : added rms gyro rates to residual logging

--- MultiRotor/Config/options.h
+++ MultiRotor/Config/options.h
@@ -1,6 +1,6 @@
 
 
-#define DATE "Wolf_pac_2 , firmware 5.2, 3/20/2024\r\n"
+#define DATE "Wolf_pac_2 , firmware 5.2, 3/27/2024\r\n"
 //#define DATE "Wold_pac_1, rev 2.5, 3/6/2023\r\n"
 
 //#define SIMULATED_GYRO
@@ -22,9 +22,9 @@
 //#define LOG_IMU_WP1               // logs IMU data during a run for wolf_pac version 1
 #define LOG_IMU_WP2               // logs IMU data during a run for wolf_pac version 2
                                     // you will also need to turn on the following:
-//#define NORMAL_RUN // this one must be turned on for normal operation 100 hz
+#define NORMAL_RUN // this one must be turned on for normal operation 100 hz
 //#define START_TRACK_LOG         // logs 2 components of force and pitch
-//#define LOG_PITCH_RATE
+#define LOG_PITCH_RATE
 #define LOG_RESIDUALS         // logs residual offsets between runs
 //#define RESIDUAL_LOG_PERIOD 30  // 2 times per minute
 //#define RESIDUAL_LOG_PERIOD 15  // 4 times per minute
@@ -52,7 +52,7 @@
 //#define SPECTRAL_ANALYSIS_BURST // 5 hz, start at 15, remember to also uncomment the following line
 //#define SAMPLES_PER_BURST 80 
 //#define SPECTRAL_ANALYSIS_CONTINUOUS
-#define TEST_SLED // set logging Hz to 200 start at 15
+//#define TEST_SLED // set logging Hz to 200 start at 15
 //#define KUFEN // set logging Hz to 200 and start angle to 30 degrees
 
 //#define ENABLE_ESP32
@@ -82,7 +82,7 @@
 
 
 // set the logger hertz, allowable values are 1,2,4,5,10,20,25,40,50,100 or 200
-#define LOGGER_HZ	200
+#define LOGGER_HZ	100
 #define HEADER_HZ	20          // records per second during header logging
 #define SLIDE_DET_HZ	200     // computations per second to detect beginning of a run
 //#define TILT_STOP_DELAY 10      // delay in seconds to allow for a roll over
@@ -106,7 +106,7 @@
 //#define LUGE7_SN053
 //#define LUGE7_SN054
 
-#define LUGE7_SN080
+//#define LUGE7_SN080
 //#define LUGE7_SN081
 //#define LUGE7_SN082
 //#define LUGE7_SN083
@@ -222,7 +222,7 @@
 
 //#define LUGE7_SN194
 //#define LUGE7_SN195
-//#define LUGE7_SN196
+#define LUGE7_SN196
 //#define LUGE7_SN197
 
 
--- MultiRotor/telemetry.c
+++ MultiRotor/telemetry.c
@@ -180,7 +180,7 @@ void send_residual_data(void)
 		start_residuals = 0 ;
 #ifndef LOG_R_UPDATE
 #ifndef TILT_INIT
-		serial_output("\r\n\r\nimu_temp_yy,filter_en_yy,x_force_yy,y_force_yy,z_force_yy,x_rate_yy,y_rate_yy,z_rate_yy,x_filt_16_yy,y_filt_16_yy,z_filt_16_yy\r\n") ;
+		serial_output("\r\n\r\nimu_temp_yy,filter_en_yy,x_force_yy,y_force_yy,z_force_yy,x_rate_yy,y_rate_yy,z_rate_yy,rms_rate_yy,x_filt_16_yy,y_filt_16_yy,z_filt_16_yy\r\n") ;
 #else
         serial_output("\r\n\r\nStandbymode\r\naccOn,logOn,nx_force,y_force,z_force,yaw8,pitch8,roll8,yaw,pitch,roll\r\n");        
 #endif // TILT_INIT
@@ -204,7 +204,7 @@ void send_residual_data(void)
 
 #ifndef  LOG_R_UPDATE 
 #ifndef TILT_INIT
-		serial_output("%i,%i,%.1f,%.1f,%.1f,%i,%i,%i,%i,%i,%i\r\n",
+		serial_output("%i,%i,%.1f,%.1f,%.1f,%i,%i,%i,%i,%i,%i,%i\r\n",
                 mpu_temp.value,
 				accelOn ,
                 ((double)(aero_force[0]))/ACCEL_FACTOR ,
@@ -213,6 +213,7 @@ void send_residual_data(void)
     			omegagyro[0],
                 omegagyro[1],
                 omegagyro[2],
+                vector3_mag(omegagyro[0],omegagyro[1],omegagyro[2]),
 				(int16_t)((omegagyro_filtered[0].WW)>>12) , // 16x
 				(int16_t)((omegagyro_filtered[1].WW)>>12) ,
 				(int16_t)((omegagyro_filtered[2].WW)>>12) 
